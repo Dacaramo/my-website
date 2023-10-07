4e92fb993850d1c0002e33334dbe7bf8b14d68a4
Dacaramo <dankramirez@outlook.com>
2023-10-07 15:19:14 -0500

    refactor: Added all needed fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundColor: {
+        white: '#FFFFFF',
+      },
+      fontFamily: {
+        sans: ['var(--font-rubik)'],
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
