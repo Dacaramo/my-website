d456eecd7fefe06af053010effe503d016439856
Dacaramo <dankramirez@outlook.com>
2023-10-07 16:18:30 -0500

    style: Added needed colors

---
+++ app/constants/colors.ts
@@ -0,0 +1,6 @@
+export const WHITE = '#FFFFFF';
+export const PALE_ACID_GREEN = '#DAF4B5';
+export const ACID_GREEN = '#AFFC41';
+export const PALE_PRUNE = '#4E2156';
+export const PRUNE = '#3C1642';
+export const BLACK = '#000000';
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      backgroundColor: {
+      colors: {
         white: '#FFFFFF',
+        'pale-acid-green': '#DAF4B5',
+        'acid-green': '#AFFC41',
+        'pale-prune': '#4E2156',
+        prune: '#3C1642',
+        'pale-black': '#1D1A1A',
+        black: '#000000',
       },
       fontFamily: {
         sans: ['var(--font-rubik)'],
